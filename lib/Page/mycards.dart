282f7f87faac7ac9fd41bcab2347bd2b4d58b2aa
Danish <danishhaqim80@gmail.com>
2025-06-21 02:23:48 +0800

    added home page

---
+++ lib/Page/mycards.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mycards extends StatelessWidget {
+  const Mycards({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.blue,
+        child: const Center(
+          child: Text('Home'),
+        )
+    );
+  }
+}
---
+++ lib/Page/statistics.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Statistics extends StatelessWidget {
+  const Statistics({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.green,
+        child: const Center(
+          child: Text('Home'),
+        )
+    );
+  }
+}

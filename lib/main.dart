5d310e4cab9e688f581b3612fd28b1a75c0ade2d
Danish <danishhaqim80@gmail.com>
2025-06-27 02:19:14 +0800

    added navigator to profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:app_banking/Page/bottomnav.dart';
-import 'package:app_banking/Page/profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Profile(),
+      home: Bottomnav(),
     );
   }
 }
